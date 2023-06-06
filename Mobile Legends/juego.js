d605ed60cbc9ed70307ccef169740b6fa00e3103
DarioMLBB23 <132954386+DarioMLBB23@users.noreply.github.com>
2023-06-05 22:07:09 -0300

    Add files via upload

---
+++ Mobile Legends/juego.js
@@ -0,0 +1,19 @@
+let nombreIngresado = prompt ("Ingresa tu nombre") 
+   alert ("Bienvenido" + nombreIngresado + "a Mundo Mobile Legends")
+
+let unNumero = prompt ("Ingresa un numero")
+
+if (unNumero < 100) {
+    console.log ("Tu línea de la suerte es Mid");
+    alert ("Tu línea de la suerte es Mid");
+}
+
+else if (unNumero > 100) {
+    console.log ("Tu linea de la suerte es jungla");
+    alert ("Tu línea de la suerte es jungla");
+}
+
+else {
+    console.log ("Pon un número, troll");
+    alert ("Pon un número, troll");
+}
